ef1114feba914d045e64414d394953669e1c90ee
Rahul vyas <rahulvyas.1144@gmail.com>
2026-01-16 19:17:58 +0530

    Remove non-existent core app

--- ems/ems/urls.py
+++ ems/ems/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
     path('', include('accounts.urls')),
     path('', include('employees.urls')),
     path('', include('leave_app.urls')),
